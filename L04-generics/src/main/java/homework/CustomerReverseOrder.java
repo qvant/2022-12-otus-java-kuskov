fd8900bf2122c7256259a2d59e00d0fb523a9aa0
Andrew Kuskov <qvant86@gmail.com>
2023-02-14 02:01:02 +0300

    very first template

---
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -0,0 +1,25 @@
+package homework;
+
+
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.TreeMap;
+
+public class CustomerReverseOrder {
+
+    //todo: 2. надо реализовать методы этого класса
+    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final ArrayDeque<Customer> arrayDeque;
+
+    public CustomerReverseOrder() {
+        this.arrayDeque = new ArrayDeque<>();
+    }
+
+    public void add(Customer customer) {
+        arrayDeque.add(customer);
+    }
+
+    public Customer take() {
+        return arrayDeque.pollLast();
+    }
+}
---
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -0,0 +1,42 @@
+package homework;
+
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CustomerService {
+
+    //todo: 3. надо реализовать методы этого класса
+    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+
+    private final TreeMap<Customer, String> map;
+
+    public CustomerService() {
+        this.map = new TreeMap<>(Comparator.comparing(Customer::getScores));
+    }
+
+    public Map.Entry<Customer, String> getSmallest() {
+        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
+        Map.Entry<Customer, String> customerStringEntry = map.firstEntry();
+        TreeMap<Customer, String> treeMap = new TreeMap<>(Comparator.comparing(Customer::getScores));
+        if (customerStringEntry != null){
+            treeMap.put(new Customer(customerStringEntry.getKey().getId(), customerStringEntry.getKey().getName(), customerStringEntry.getKey().getScores()), customerStringEntry.getValue());
+        }
+        return treeMap.firstEntry();
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        Map.Entry<Customer, String> customerStringEntry = map.higherEntry(customer);
+
+        TreeMap<Customer, String> buf = new TreeMap<>(Comparator.comparing(Customer::getScores));
+        if (customerStringEntry != null){
+            buf.put(new Customer(customerStringEntry.getKey().getId(), customerStringEntry.getKey().getName(), customerStringEntry.getKey().getScores()), customerStringEntry.getValue());
+        }
+        return buf.firstEntry();
+    }
+
+    public void add(Customer customer, String data) {
+        map.put(customer, data);
+    }
+}
